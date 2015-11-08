0953ac87de18bd2bb7e648fc0776fb47b6eddf02
stasha <contact@stasha.info>
2015-11-08 22:22:11 +0100

    - removed bower_components - added map for minified source

--- .minify.js
+++ .minify.js
@@ -7,6 +7,7 @@ new compressor.minify({
     //type: 'yui-js',
     fileIn: 'src/xvalidate.js',
     fileOut: 'src/xvalidate.min.js',
+    options: ['--create_source_map=src/xvalidate.min.js.map'],
     callback: function(err, min){
         console.log(err);
 //        console.log(min);
